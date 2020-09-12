a316300b4386aa93130aa8ac1c13ccc63760be7f
Dimitar <dimitar_dimitrov12@mail.bg>
2020-09-12 18:38:05 +0300

    Updates

---
+++ precache-manifest.3d4ffde3112fe31f04fc8c074a4ed669.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10cdce7a6a8ee05cae379f24f085cac3",
+    "url": "/React-website-2/index.html"
+  },
+  {
+    "revision": "1a4f6dcfb9171ab43488",
+    "url": "/React-website-2/static/js/2.7d17676e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-website-2/static/js/2.7d17676e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3589fd0fa2965525eff6",
+    "url": "/React-website-2/static/js/main.552ecc8b.chunk.js"
+  },
+  {
+    "revision": "8136edd5067a9295b6b1",
+    "url": "/React-website-2/static/js/runtime-main.96e08c39.js"
+  },
+  {
+    "revision": "79ba2002d56019901132986d5c84067a",
+    "url": "/React-website-2/static/media/profile.79ba2002.jpg"
+  },
+  {
+    "revision": "55cfb4817b9e054491bd9dafc21087c2",
+    "url": "/React-website-2/static/media/svg-1.55cfb481.svg"
+  },
+  {
+    "revision": "d4846e1163a89864dd49fb03ec6308ed",
+    "url": "/React-website-2/static/media/svg-2.d4846e11.svg"
+  },
+  {
+    "revision": "397c455a49d4f872c292a3dc25432579",
+    "url": "/React-website-2/static/media/svg-3.397c455a.svg"
+  }
+]);
